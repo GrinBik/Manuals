9d2a4105a54db0fdbe623c33839492d88d2ba20c
GrinBik <gregorybik@gmail.com>
2024-01-23 21:53:48 +0400

    Homework is absolutely completed.

--- Manuals_(second)/manual_28_discord_bot/homework.py
+++ Manuals_(second)/manual_28_discord_bot/homework.py
@@ -1,6 +1,8 @@
 # ДЗ
 # Разобраться с аннотациями и классами.
 # Используя подсказки, посмотреть что есть внутри класса Message и его внутренних классов.
+# Пусть бот пишет администратору в ЛС, если кто-то из пользователей пишет в чате что-нибудь со словом «помощь».
+# Сообщение администратору должно выглядеть так: Пользователь {юзернейм} просит помощи на сервере {имя сервера}!
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -17,9 +19,14 @@ async def on_message(message: discord.Message):
     if message.author.bot:
         return
     print(message)
-    await message.channel.send("Hello!")
     print(message.channel)
     print(message.author)
+    guild = message.guild
+    if "помощь" in message.content:
+        for admin in guild.members:
+            if admin.guild_permissions.administrator and admin.name == 'grinbik':
+                await admin.send(f'Пользователь {message.author} просит помощи на сервере {guild}!')
+
 
 
 bot.run(TOKEN)
