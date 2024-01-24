36c3c39369452601a9ca9eea20b634536e819476
GrinBik <gregorybik@gmail.com>
2024-01-24 22:40:32 +0400

    Upgrading from Python version 3.12 to version 3.10.8. This change allowed transitioning from the "discord" module to the "py-cord" module.

--- Manuals_(second)/manual_28_discord_bot/homework.py
+++ Manuals_(second)/manual_28_discord_bot/homework.py
@@ -4,14 +4,13 @@
 # Пусть бот пишет администратору в ЛС, если кто-то из пользователей пишет в чате что-нибудь со словом «помощь».
 # Сообщение администратору должно выглядеть так: Пользователь {юзернейм} просит помощи на сервере {имя сервера}!
 import discord
-from discord.ext import commands
 from dotenv import load_dotenv
 import os
 
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+bot = discord.Bot(intents=discord.Intents.all())
 
 
 @bot.event
@@ -25,8 +24,7 @@ async def on_message(message: discord.Message):
     if "помощь" in message.content:
         for admin in guild.members:
             if admin.guild_permissions.administrator and admin.name == 'grinbik':
-                await admin.send(f'Пользователь {message.author} просит помощи на сервере {guild}!')
-
+                await admin.send(f'Пользователь {message.author.name} просит помощи на сервере {guild}!')
 
 
 bot.run(TOKEN)
--- Manuals_(second)/manual_28_discord_bot/manual.py
+++ Manuals_(second)/manual_28_discord_bot/manual.py
@@ -1,12 +1,11 @@
 import discord
-from discord.ext import commands
 from dotenv import load_dotenv
 import os
 
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+bot = discord.Bot(intents=discord.Intents.all())
 
 
 @bot.event
