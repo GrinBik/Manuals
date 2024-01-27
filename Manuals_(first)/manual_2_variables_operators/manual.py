b50c68254d10558f2061544df94531949d24e9e5
GrinBik <gregorybik@gmail.com>
2024-01-27 09:59:07 +0400

    Some changes have been done in second manual.

---
+++ Manuals_(first)/manual_2_variables_operators/manual.py
@@ -0,0 +1,62 @@
+apple = 4
+
+a = 10
+sasha = 12
+doggy = 14
+kilos = 7
+
+gravity = 9.8
+pi = 3.14
+apple = 0.5
+android = 13.4
+
+name = "Саша"
+color = "Красный"
+
+name = 'Katya'
+second = 'Ivanova'
+data = 'Сегодня 31 декабря! С наступающим!'
+
+a = True
+a = False
+
+print(type(name))
+
+name = input()
+print(name)
+
+a = 5
+print(a + 3)
+a = 10 / 2
+print(a)
+
+print(5 / 2)
+print(5 // 2)
+
+print(5 / 2)
+print(5 % 2)
+
+apple = 5
+apple += 3
+print(apple)
+
+sklad = 0
+for_day = 15000 - 13500
+sklad += for_day + for_day + for_day + for_day + for_day + for_day + for_day
+print(sklad)
+
+sklad = 0
+for_day = 15000 - 13500
+sklad += for_day * 7
+print(sklad)
+
+stroka = '34'
+print(type(int(stroka)))
+
+age = input()
+age = int(age)
+print(2023 - age)
+
+num = 'Четыре'
+apples = 'яблока'
+print(num + apples)
