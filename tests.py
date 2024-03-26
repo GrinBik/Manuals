af76a536935fa738f7f0a4991ada7fd642ab210a
GrinBik <gregorybik@gmail.com>
2024-03-26 09:43:24 +0200

    Different tests had been done.

--- tests.py
+++ tests.py
@@ -2,12 +2,29 @@ import s_taper
 from s_taper.consts import *
 
 
-scheme = {
-    'user_id': INT + KEY,
-    'name': TEXT,
-    'rating': INT
-}
+def check(line):
+    if line == 'INT':
+        return INT
+    elif line == 'FLT':
+        return FLT
+    elif line == 'TEXT':
+        return TEXT
+    elif line == 'BLN':
+        return BLN
 
-users = s_taper.Taper('users', 'data.db').create_table(scheme)
 
-user = users.read('user_id', 0)
+name = input('Введите название таблицы: ')
+columns = input('Введите название каждого столбца и тип его данных через запятую: ').split(',')
+n = len(columns)
+scheme = {}
+print(f'len = {n}')
+for i in range(0, n, 2):
+    # scheme[columns[i]] = check(columns[i+1])
+    scheme[columns[i]] = columns[i + 1]
+    # print(columns[_])
+for j in list(scheme.values()):
+    print(type(j))
+
+tests = s_taper.Taper(name, 'data.db').create_table(scheme)
+
+print(scheme)
