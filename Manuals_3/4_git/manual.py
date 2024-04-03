258db88621864a498e90cb3ded6a91437298504b
GrinBik <gregorybik@gmail.com>
2024-04-03 17:24:40 +0200

    Написано задание, переделана функция.

--- Manuals_3/4_git/manual.py
+++ Manuals_3/4_git/manual.py
@@ -1,3 +1,6 @@
+# Модернизировать программу таким образом, чтобы игра продолжалась только с нечётными числами.
+# Написать функцию, которая будет отсеивать чётные числа из игры.
+# Сделать коммит изменений и загрузить их в GitHub (сделать все через pyCharm).
 import random
 
 points = {
@@ -6,19 +9,19 @@ points = {
 }
 
 
-def is_odd(number: int):
+def is_even_number(number: int):
     if number % 2 == 0:
-        return False
-    else:
         return True
+    else:
+        return False
 
 
 while True:
     a = random.randint(10, 110)
-    while not is_odd(a):
+    while is_even_number(a):
         a = random.randint(10, 110)
     b = random.randint(10, 120)
-    while not is_odd(b):
+    while is_even_number(b):
         b = random.randint(10, 110)
     op = random.choice(("+", "-"))
     if op == "+":
