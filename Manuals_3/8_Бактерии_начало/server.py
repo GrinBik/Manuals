06ebc7d22fe5ec3013698bf24a0fe3113e99780b
GrinBik <gregorybik@gmail.com>
2024-06-14 15:08:51 +0200

    Переосмысленная и измененная методичка 8.

--- Manuals_3/8_Бактерии_начало/server.py
+++ Manuals_3/8_Бактерии_начало/server.py
@@ -1,36 +1,33 @@
 import socket
 import time
 
-# настраиваем главный сокет
+# создаем главный сокет socket.AF_INET - семейство адресов IPv4, socket.SOCK_STREAM - протокол TCP
 main_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 # отключаем пакетирование
 main_socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
-
-# установка IP-адреса и порта (соединение)
+# связываем сокет с конкретным портом (IP-адрес и порт)
 main_socket.bind(("localhost", 10000))
-
-# не закрывать данный сокет после соединения
+# чтобы программа не останавливалась пока ждет нового подключения, работала дальше
 main_socket.setblocking(False)
-
-# кол-во одновременно слушаемых игроков
+# кол-во одновременных запросов на подключение
 main_socket.listen(5)
-
-print("Сокет готов")
+print("Главный сокет готов")
 
 # список игроков
 players = []
 
 # сервер должен работать постоянно
 while True:
+    # принимаем входящие запросы
     try:
-        # принимаем входящие запросы: new_socket - подключение (сокет клиента), addr - клиент (его id)
+        # new_socket - выделенный сокет (порт) игроку (новый), addr - адрес игрока
         new_socket, addr = main_socket.accept()
-        print('Подключился', addr)
-        # не закрывать данный сокет после соединения
+        # чтобы программа не останавливалась пока ждет команды от игрока, работала дальше
         new_socket.setblocking(False)
-        # добавляем сокет нового игрока в список
+        # добавляем сокет нового игрока в список игроков
         players.append(new_socket)
+        # вывод информации о подключившемся игроке
+        print('Подключился ', addr)
     except BlockingIOError:
         pass
 
@@ -38,8 +35,9 @@ while True:
     for sock in players:
         try:
             # 1024 - это кол-во получаемых данных, которые декодируем из байтов в строку
-            data = sock.recv(1024).decode()
-            print("Получено сообщение: ", data)
+            data = sock.recv(34)
+            data = data.decode()
+            print("Получено: ", data)
         except:
             pass
 
